93be2fbdac9c5cfeb9c910a923dd0cfc373413d8
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2021-10-07 18:38:33 +1100

    adding autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ozdex'
-copyright = '2021, Robert Turnbull and Jonathan Garber'
-author = 'Robert Turnbull and Jonathan Garber'
+project = "ozdex"
+copyright = "2021, Robert Turnbull and Jonathan Garber"
+author = "Robert Turnbull and Jonathan Garber"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = "0.1.4"
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,19 +32,22 @@ release = '0.1.4'
 # ones.
 extensions = [
     "sphinx_rtd_theme",
-    'nbsphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages',
-    'myst_parser',
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.githubpages",
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -52,9 +55,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
