cfc18830a390ca3c0206af4b0f21722785f73643
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2021-11-11 17:26:40 +1100

    adding more tests for date function

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -3,6 +3,11 @@ from unittest.mock import patch
 from pathlib import Path
 from datetime import datetime
 import numpy as np
+import pandas as pd
+import modin.pandas as mpd
+import modin.config as cfg
+
+cfg.IsDebug.put(True)
 
 from ausdex.dates import convert_date
 
@@ -23,9 +28,36 @@ class TestDates(unittest.TestCase):
             convert_date(2006.5), np.array("2006-07-02", dtype="datetime64[D]")
         )
 
+    def test_datetime(self):
+        np.testing.assert_equal(
+            convert_date(datetime(2006, 7, 2)),
+            np.array("2006-07-02", dtype="datetime64[D]"),
+        )
+
     def test_numpy_ints(self):
         dates = np.array([2006, 2007], dtype=np.int32)
         np.testing.assert_equal(
             convert_date(dates),
             np.array(["2006-01-01", "2007-01-01"], dtype="datetime64[D]"),
+        )
+
+    def test_numpy_dates(self):
+        dates = np.array(["2006-05-06", "2007-02-04"], dtype="datetime64[D]")
+        np.testing.assert_equal(
+            convert_date(dates),
+            np.array(["2006-05-06", "2007-02-04"], dtype="datetime64[D]"),
+        )
+
+    def test_pandas_series(self):
+        dates = pd.Series(["Jul 31, 2009", "2010-01-10", datetime(2006, 3, 1)])
+        np.testing.assert_equal(
+            convert_date(dates),
+            np.array(["2009-07-31", "2010-01-10", "2006-03-01"], dtype="datetime64[D]"),
+        )
+
+    def test_modin_series(self):
+        dates = mpd.Series(["Jul 31, 2009", "2010-01-10", datetime(2006, 3, 1)])
+        np.testing.assert_equal(
+            convert_date(dates),
+            np.array(["2009-07-31", "2010-01-10", "2006-03-01"], dtype="datetime64[D]"),
         )
