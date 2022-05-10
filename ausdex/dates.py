:zap: fixing date to decimal year calculation to make it more accurate and updating tests. 100% coverage now

ausdex/dates.py
@@ -15,7 +15,7 @@ def convert_date(date: Union[datetime, str, pd.Series, np.ndarray]) -> np.ndarra
         date (Union[datetime, str, pd.Series, np.ndarray]): The date to be converted
 
     Returns:
-        np.ndarray: A numerical form of the date.
+        np.ndarray: A NumPy array with datatype datetime64[D].
     """
     if isinstance(date, int):
         date = pd.to_datetime(str(date))
@@ -35,8 +35,8 @@ def convert_date(date: Union[datetime, str, pd.Series, np.ndarray]) -> np.ndarra
     return np.array(date, dtype="datetime64[D]")
 
 
-def _dt_to_dyr(x):
-    return x.year + x.month / 12
+def timestamp_to_decimal_year(date):
+    return np.array(date.year + (date.dayofyear - 1) / (365.0 + date.is_leap_year * 1.0))
 
 
 def date_time_to_decimal_year(
@@ -53,19 +53,29 @@ def date_time_to_decimal_year(
         np.ndarray,
     ]
 ) -> np.ndarray:
+    """
+    Converts a date from a variety of formats to be a decimal year.
+
+    Args:
+        date (Union[ datetime, pd.Timestamp, list, tuple, np.datetime64, int, float, str, pd.Series, np.ndarray, ]):
+            The date to be converted.
+
+    Returns:
+        np.ndarray: The date as a NumPy array of the same shape as the input.
+    """
+    # If the date is a list or a tuple, then convert to a numpy array before doing anything else
     if isinstance(date, (list, tuple)):
         date = np.array(date)
+
     if isinstance(date, (float, int)):
+        # if a scalar numerical value, then assume that this is already as a numerical date
         return np.array([date])
     elif isinstance(date, (datetime, pd.Timestamp, np.datetime64)):
-        return _dt_to_dyr(pd.to_datetime(date))
-
-    elif isinstance(date, (pd.Series, mpd.Series, np.ndarray, list, tuple)):
+        # if a scalar date value, then convert to pandas to be converted to decimal year
+        return timestamp_to_decimal_year(pd.to_datetime(date))
+    elif isinstance(date, (pd.Series, mpd.Series, np.ndarray)):
         if date.dtype in [float, int]:
+            # if it is already an array of numerical values, then just return it
             return date
-        else:
-            return np.array(_dt_to_dyr(pd.to_datetime(convert_date(date))))
 
-    if type(date) != pd.Timestamp:
-        date = pd.Timestamp(date)
-    return _dt_to_dyr(date)
+    return timestamp_to_decimal_year(pd.to_datetime(convert_date(date)))

tests/test_dates.py
@@ -19,14 +19,10 @@ class TestDates(unittest.TestCase):
         np.testing.assert_equal(result, np.array("2006-01-01", dtype="datetime64[D]"))
 
     def test_int(self):
-        np.testing.assert_equal(
-            convert_date(2006), np.array("2006-01-01", dtype="datetime64[D]")
-        )
+        np.testing.assert_equal(convert_date(2006), np.array("2006-01-01", dtype="datetime64[D]"))
 
     def test_float(self):
-        np.testing.assert_equal(
-            convert_date(2006.5), np.array("2006-07-02", dtype="datetime64[D]")
-        )
+        np.testing.assert_equal(convert_date(2006.5), np.array("2006-07-02", dtype="datetime64[D]"))
 
     def test_datetime(self):
         np.testing.assert_equal(
@@ -74,6 +70,22 @@ class TestDateToDecimalYear(unittest.TestCase):
         self.assertIsInstance(result, np.ndarray)
         self.assertEqual(result, 1995.5)
 
+    def test_float_list(self):
+        result = date_time_to_decimal_year([1995.5])
+        self.assertIsInstance(result, np.ndarray)
+        self.assertEqual(result, 1995.5)
+
+    def test_datetime(self):
+        result = date_time_to_decimal_year(datetime(1996, 7, 2))
+        self.assertIsInstance(result, np.ndarray)
+        self.assertEqual(result, 1996.5)
+
+    def test_float_tuple(self):
+        result = date_time_to_decimal_year((1995.5, 1996.6))
+        self.assertIsInstance(result, np.ndarray)
+        self.assertEqual(result[0], 1995.5)
+        self.assertEqual(result[1], 1996.6)
+
     def test_float_array(self):
         result = date_time_to_decimal_year(np.array([1995.5, 1996.6]))
         self.assertIsInstance(result, np.ndarray)
@@ -85,57 +97,47 @@ class TestDateToDecimalYear(unittest.TestCase):
         self.assertTrue(all([x == y for x, y in zip(result, [1995, 1996])]))
 
     def test_datetime_array(self):
-        result = date_time_to_decimal_year(
-            np.array([np.datetime64("1995-01-01"), np.datetime64("1996-06-30")])
-        )
+        result = date_time_to_decimal_year(np.array([np.datetime64("1995-01-01"), np.datetime64("1996-07-02")]))
         self.assertIsInstance(result, np.ndarray)
         # trues = [x == y for x, y in zip(result, [1995.5, 1996.6])]
-        self.assertAlmostEqual(result[0], 1995.0833, 4)
+        self.assertAlmostEqual(result[0], 1995.0, 4)
         self.assertEqual(result[1], 1996.5)
 
     def test_str_array(self):
-        result = date_time_to_decimal_year(np.array(["1995-01-01", "1996-06-30"]))
+        result = date_time_to_decimal_year(np.array(["1995-01-01", "1996-07-02"]))
         self.assertIsInstance(result, np.ndarray)
         # trues = [x == y for x, y in zip(result, [1995.5, 1996.6])]
-        self.assertAlmostEqual(result[0], 1995.0833, 4)
+        self.assertAlmostEqual(result[0], 1995.0, 4)
         self.assertEqual(result[1], 1996.5)
 
     def test_str_series(self):
-        result = date_time_to_decimal_year(
-            pd.Series(np.array(["1995-01-01", "1996-06-30"]))
-        )
+        result = date_time_to_decimal_year(pd.Series(np.array(["1995-01-01", "1996-07-02"])))
         self.assertIsInstance(result, np.ndarray)
         # trues = [x == y for x, y in zip(result, [1995.5, 1996.6])]
-        self.assertAlmostEqual(result[0], 1995.0833, 4)
+        self.assertAlmostEqual(result[0], 1995.0, 4)
         self.assertEqual(result[1], 1996.5)
 
     def test_datetime_series(self):
         result = date_time_to_decimal_year(
-            pd.Series(
-                np.array([np.datetime64("1995-01-01"), np.datetime64("1996-06-30")])
-            )
+            pd.Series(np.array([np.datetime64("1995-01-01"), np.datetime64("1996-07-02")]))
         )
         self.assertIsInstance(result, np.ndarray)
         # trues = [x == y for x, y in zip(result, [1995.5, 1996.6])]
-        self.assertAlmostEqual(result[0], 1995.0833, 4)
+        self.assertAlmostEqual(result[0], 1995.0, 4)
         self.assertEqual(result[1], 1996.5)
 
     def test_datetime_mpdseries(self):
         result = date_time_to_decimal_year(
-            mpd.Series(
-                np.array([np.datetime64("1995-01-01"), np.datetime64("1996-06-30")])
-            )
+            mpd.Series(np.array([np.datetime64("1995-01-01"), np.datetime64("1996-07-02")]))
         )
         self.assertIsInstance(result, np.ndarray)
         # trues = [x == y for x, y in zip(result, [1995.5, 1996.6])]
-        self.assertAlmostEqual(result[0], 1995.0833, 4)
+        self.assertAlmostEqual(result[0], 1995.0, 4)
         self.assertEqual(result[1], 1996.5)
 
     def test_str_mpdseries(self):
-        result = date_time_to_decimal_year(
-            mpd.Series(np.array(["1995-01-01", "1996-06-30"]))
-        )
+        result = date_time_to_decimal_year(mpd.Series(np.array(["1995-01-01", "1996-07-02"])))
         self.assertIsInstance(result, np.ndarray)
         # trues = [x == y for x, y in zip(result, [1995.5, 1996.6])]
-        self.assertAlmostEqual(result[0], 1995.0833, 4)
+        self.assertAlmostEqual(result[0], 1995.0, 4)
         self.assertEqual(result[1], 1996.5)