e24bafdc2b56950cb8e34dec0591bc2571e46222
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-04-06 15:57:44 +1000

    :zap: removing debugging print statement

--- ausdex/files.py
+++ ausdex/files.py
@@ -42,8 +42,6 @@ def cached_download(url: str, local_path: Union[str, Path], force: bool = False,
     local_path = Path(local_path)
     if (not local_path.exists() or local_path.stat().st_size == 0) or force:
         try:
-            if verbose:
-                print(f"Downloading {url} to {local_path}")
             urllib.request.urlretrieve(url, local_path)
         except Exception:
             raise DownloadError(f"Error downloading {url}")
