2feb598756267b1d7c3d99eac16821ade00a8c67
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2024-02-14 16:57:02 +1100

    :cop: updating expected files

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -8,21 +8,20 @@ def test_inflation_graph(check_viz):
     fig = check_viz.plot_inflation_timeseries("01-01-2019", start_date="06-06-1949", end_date=2020)
     with tempfile.NamedTemporaryFile(suffix=".json") as tmp:
         fig.write_json(tmp.name)
-
-        assert filecmp.cmp(
-            "tests/testdata/ausdex/test_inflation_fig.json",
-            tmp.name,
-        )
+        expected = "tests/testdata/ausdex/test_inflation_fig.json"
+        # shutil.copy(tmp.name, expected) # hack to copy the file to the expected location
+        if not filecmp.cmp(expected, tmp.name):
+            raise ValueError("test_inflation_fig.json not the same as expected")
 
 
 def test_cpi_graph(check_viz):
     fig = check_viz.plot_cpi_timeseries(start_date="06-06-1949", end_date=2020)
     with tempfile.NamedTemporaryFile(suffix=".json") as tmp:
         fig.write_json(tmp.name)
-        assert filecmp.cmp(
-            "tests/testdata/ausdex/test_cpi_fig.json",
-            tmp.name,
-        )
+        expected = "tests/testdata/ausdex/test_cpi_fig.json"
+        # shutil.copy(tmp.name, expected) # hack to copy the file to the expected location
+        if not filecmp.cmp(expected, tmp.name):
+          raise ValueError("test_cpi_fig.json not the same as expected")
 
 
 def test_plot_cpi_change(check_viz):
@@ -30,8 +29,6 @@ def test_plot_cpi_change(check_viz):
     with tempfile.NamedTemporaryFile(suffix=".json") as tmp:
         fig.write_json(tmp.name)
         expected = "tests/testdata/ausdex/test_plot_cpi_change.json"
-        # shutil.copy(tmp.name, expected)
-        assert filecmp.cmp(
-            expected,
-            tmp.name,
-        )
+        # shutil.copy(tmp.name, expected) # hack to copy the file to the expected location
+        if not filecmp.cmp(expected, tmp.name):
+            raise ValueError("test_plot_cpi_change.json not the same as expected")
